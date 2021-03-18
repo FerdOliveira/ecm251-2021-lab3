ad48bc18fe08379f65ac56a93ef95f0b4507f04f
Fernando <ferd.olvr@gmail.com>
2021-03-18 09:14:07 -0300

    Aula05-teoria

---
+++ Aula05-teoria/src/fernando/oliveira/Main.java
@@ -0,0 +1,9 @@
+package fernando.oliveira;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cpf = "425-510-838-26";
+        System.out.println("Cpf valido:"+ ValidadorCPF.validarCpf(cpf));
+    }
+}
