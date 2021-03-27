cdbe26e8e6780370fcca70d4cf76740dc1c65319
Fernando <ferd.olvr@gmail.com>
2021-03-27 16:54:17 -0300

    Atividade1_TesteDeUsuarios
    Criado exemplos de novos usuários apenas para saber se está rodando

--- Atividade1/src/fernando/oliveira/Main.java
+++ Atividade1/src/fernando/oliveira/Main.java
@@ -3,6 +3,11 @@ package fernando.oliveira;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Usuarios usuario1 = new Usuarios("Murilo Zanini","123321","Zanini@maua.com");
+	Usuarios usuario2 = new Usuarios("Ferd","123123","Ferd@maua.com");
+	Usuarios usuario3 = new Usuarios("Scalco","321321","Scalco@maua.com");
+	System.out.println(usuario1);
+	System.out.println(usuario2);
+	System.out.println(usuario3);
     }
 }
--- Atividade1/src/fernando/oliveira/Usuarios.java
+++ Atividade1/src/fernando/oliveira/Usuarios.java
@@ -4,4 +4,18 @@ public class Usuarios {
     private String nome;
     private String senha;
     private String email;
+
+    public Usuarios(String nome, String senha, String email) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+    @Override
+    public String toString() {
+        return "Seu nome é: " +
+                  nome +
+                ", com a senha:" + senha +
+                ", e e-mail "+ email.toString();
+    }
+
 }
