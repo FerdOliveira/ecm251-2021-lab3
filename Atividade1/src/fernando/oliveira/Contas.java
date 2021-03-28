58bb40d186be75d9298105e56cd39c25b4c99d5c
Fernando <ferd.olvr@gmail.com>
2021-03-28 12:36:04 -0300

    Atividade1_ClasseContasDep/Sac
    Mesmo nao tendo operações para a mesma diretamente, indiretamente precisarei em transacoes.

--- Atividade1/src/fernando/oliveira/Contas.java
+++ Atividade1/src/fernando/oliveira/Contas.java
@@ -5,6 +5,17 @@ public class Contas {
     private double saldo;
     private int IdDaVez = 1;
 
+    public void depositar(double valor){ //Mesmo não tendo operação para depositar, sei que vou ter que usar o metodo depositar para colocar o dinheiro de alguem em transacoes e por o saldo ser privado preciso fazer aqui
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor){ //Mesmo não tendo operação para sacar, sei que vou ter que usar o metodo sacar para tirar o dinheiro de alguem em transacoes e por o saldo ser privado preciso fazer aqui
+        if(valor >= this.saldo ){
+            this.saldo -= valor;
+            return true;
+        } return false;
+    }
+
     public int getIdConta(){ //metodo criado para pegar o id da conta já que dito iremos utilizar ele em transacoes
         return this.idConta;
     }
