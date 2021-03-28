76a4ff2190cef3d97fdbd8dc5fce87e9d4bcf76d
Fernando <ferd.olvr@gmail.com>
2021-03-28 16:15:23 -0300

    Atividade1_CriacaoDoConstrutorContasETeste
    Foi criado um construtor na classe Contas para então termos os dados necessarios, e recebe como parametros o usuario da vez e o saldo, o IdConta nao entra neste caso pois nao é pra ser passado como parametro e sim algo para ser indo atualizado automaticamente, após isto foi feito testes no main para ver se compilava tal codigo

--- Atividade1/src/fernando/oliveira/Contas.java
+++ Atividade1/src/fernando/oliveira/Contas.java
@@ -6,6 +6,13 @@ public class Contas {
     private static int idDaVez = 1; // foi adicionado o static para contar o id de cada conta pois, caso contrario ele nao seria um atributo compartilhado com as demais classes
     private Usuarios pessoa; // para então saber para que saldo sera atribuido precisanmos pegar a classe de usuarios que foi criada naquele momento para usar como referencia a mesma.
 
+    public Contas(Usuarios pessoa, double saldo){ //o IdConta nao entra pois é um atributo que iremos indo atualizar ele, e nao algo que precisa ser passado como parametro
+        this.pessoa = pessoa; // pegar as informações da classe Usuarios daquele que quero acessar
+        this.saldo = saldo;
+        this.idConta = idDaVez;
+        idDaVez++; //primeramente o id vale 1, mas conforme for criando este id vai para 2, 3 ....
+    }
+
     public void depositar(double valor){ //Mesmo não tendo operação para depositar, sei que vou ter que usar o metodo depositar para colocar o dinheiro de alguem em transacoes e por o saldo ser privado preciso fazer aqui
         this.saldo += valor;
     }
@@ -25,9 +32,9 @@ public class Contas {
         return this.saldo;
     }
 
-    public int getIdDaVez() { //No momento não preciso mas deixarei criado para todos um getter
+    public static int getIdDaVez() { //No momento não preciso mas deixarei criado para todos um getter
         return Contas.idDaVez; // Lembrando que aqui nao pode ser this, pois o atributo idDaVez nao deve ser algo instancia, e sim algo para a classe Contas.
-    }
+    } // e tambem nao podemos invocar nada com this dentro deste metodo.
 }
 
 
--- Atividade1/src/fernando/oliveira/Main.java
+++ Atividade1/src/fernando/oliveira/Main.java
@@ -9,5 +9,11 @@ public class Main {
 //	System.out.println(usuario1);
 //	System.out.println(usuario2);
 //	System.out.println(usuario3);
+	Contas conta1 = new Contas(usuario1,1000);
+	Contas conta2 = new Contas(usuario2,250);
+	Contas conta3 = new Contas(usuario3,3000);
+//	System.out.println(conta1.getIdConta()); //Ver se estava atualizando o ID da conta e se compialva a classe conta
+//	System.out.println(conta2.getIdConta()); //Ver se estava atualizando o ID da conta e se compialva a classe conta
+//	System.out.println(conta3.getIdConta()); //Ver se estava atualizando o ID da conta e se compialva a classe conta
     }
 }
