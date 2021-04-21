70f74c42cb69fdf42555d706340493bfe34eae5b
Fernando <ferd.olvr@gmail.com>
2021-04-21 16:26:09 -0300

    ClassePedra

--- pedra-papel-tesoura/src/fernando/oliveira/Pedra.java
+++ pedra-papel-tesoura/src/fernando/oliveira/Pedra.java
@@ -1,5 +1,18 @@
 package fernando.oliveira;
 
 public class Pedra extends Jogada{
+    @Override
+    public boolean verificaSeGanhei(Jogada jogada) {
+        return jogada instanceof Tesoura;
+    }
 
+    @Override
+    public boolean verificaSePerdi(Jogada jogada) {
+        return jogada instanceof Papel;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedra";
+    }
 }
