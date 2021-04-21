8c4da785a57d6376f6d15219e620bb66879d6e60
Fernando <ferd.olvr@gmail.com>
2021-04-21 16:29:34 -0300

    Final

--- pedra-papel-tesoura/src/fernando/oliveira/Main.java
+++ pedra-papel-tesoura/src/fernando/oliveira/Main.java
@@ -3,6 +3,7 @@ package fernando.oliveira;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Sistema sistema = new Sistema();
+        sistema.run();
     }
 }
