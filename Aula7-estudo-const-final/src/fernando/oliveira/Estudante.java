6c1e2dc8108bce814863859feeb332936b7fca21
Fernando <ferd.olvr@gmail.com>
2021-04-22 14:02:37 -0300

    Main e Estudante

--- Aula7-estudo-const-final/src/fernando/oliveira/Estudante.java
+++ Aula7-estudo-const-final/src/fernando/oliveira/Estudante.java
@@ -1,4 +1,22 @@
 package fernando.oliveira;
 
-public class Estudante {
+public class Estudante extends Pessoa{
+    private String ra;
+
+    public Estudante(String nome, String cpf, String ra) {
+        super(nome, cpf);
+        this.ra = ra;
+    }
+
+    public Estudante(String nome, String cpf) {
+        super(nome, cpf);
+    }
+
+    @Override
+    public String toString() {
+        return "Estudante{" +
+                "ra=" + ra  +
+                ", mudar=" + mudar +
+                '}';
+    }
 }
