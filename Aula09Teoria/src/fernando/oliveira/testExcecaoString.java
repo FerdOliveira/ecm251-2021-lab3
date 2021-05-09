ca100465a215aeb588d7776ff32dc946975a8d6e
Fernando <ferd.olvr@gmail.com>
2021-05-08 23:01:11 -0300

    Update testExcecaoString.java

--- Aula09Teoria/src/fernando/oliveira/testExcecaoString.java
+++ Aula09Teoria/src/fernando/oliveira/testExcecaoString.java
@@ -4,7 +4,14 @@ public class testExcecaoString {
     public static void main(String[] args) {
         String frase = null;
         String novaFrase = null;
-        novaFrase = frase.toUpperCase();
+        try {
+            novaFrase = frase.toUpperCase();
+        }
+        catch (NullPointerException exception){
+            System.out.println("Uma exceção aconteceu:"+exception.getMessage());
+            frase = "";
+            novaFrase = "";
+        }
         System.out.println("Frase Antiga:" + frase);
         System.out.println("Frase Nova:" + novaFrase);
     }
