fcd1918b4e348d0db5cf92762aae27ba5ac09468
Fernando <ferd.olvr@gmail.com>
2021-05-08 23:16:48 -0300

    Update Spoke.java

--- Aula08-lab3/src/fernando/oliveira/models/bazinga/Spoke.java
+++ Aula08-lab3/src/fernando/oliveira/models/bazinga/Spoke.java
@@ -1,4 +1,22 @@
 package fernando.oliveira.models.bazinga;
 
-public class Spoke {
-}
+import fernando.oliveira.models.Jogada;
+import fernando.oliveira.models.classico.Papel;
+import fernando.oliveira.models.classico.Pedra;
+import fernando.oliveira.models.classico.Tesoura;
+
+public class Spoke extends Jogada {
+
+    @Override
+    public Jogada[] jogadasQueGanho() {
+        return new Jogada[]{new Pedra(), new Tesoura()};
+    }
+
+    @Override
+    public Jogada[] jogadasQuePerco() {
+        return new Jogada[] {new Papel(), new Lagarto()};
+    }
+    @Override
+    public String toString() {
+        return "Spoke";
+    }
