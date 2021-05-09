598de43e198211486e4e306ac500130b3d3d34e9
Fernando <ferd.olvr@gmail.com>
2021-05-08 23:17:31 -0300

    Update Lagarto.java

--- Aula08-lab3/src/fernando/oliveira/models/bazinga/Lagarto.java
+++ Aula08-lab3/src/fernando/oliveira/models/bazinga/Lagarto.java
@@ -1,4 +1,23 @@
 package fernando.oliveira.models.bazinga;
 
-public class Lagarto {
+import fernando.oliveira.models.Jogada;
+import fernando.oliveira.models.classico.Papel;
+import fernando.oliveira.models.classico.Pedra;
+import fernando.oliveira.models.classico.Tesoura;
+
+public class Lagarto extends Jogada {
+    @Override
+    public Jogada[] jogadasQueGanho() {
+        return new Jogada[]{new Papel(), new Spoke()};
+    }
+
+    @Override
+    public Jogada[] jogadasQuePerco() {
+        return new Jogada[] {new Pedra(), new Tesoura()};
+    }
+
+    @Override
+    public String toString() {
+        return "Lagarto";
+    }
 }
