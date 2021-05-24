7ea50965719e3e6ba906d14503dcc893954796ce
Fernando <ferd.olvr@gmail.com>
2021-05-24 13:27:07 -0300

    Update LinkedListTestDrive.java

--- Aula 12/src/linked_list_test_drive/LinkedListTestDrive.java
+++ Aula 12/src/linked_list_test_drive/LinkedListTestDrive.java
@@ -1,4 +1,19 @@
 package linked_list_test_drive;
 
+import fernando.oliveira.Pessoa;
+
+import java.util.LinkedList;
+
 public class LinkedListTestDrive {
+    public static void main(String[] args) {
+        LinkedList<Pessoa> pessoas = new LinkedList<>();
+        pessoas.add(new Pessoa("Batman", "001"));
+        pessoas.add(new Pessoa("Superman", "002"));
+        pessoas.add(new Pessoa("Mutano", "056"));
+        pessoas.addFirst(new Pessoa("Ravena", "000"));
+
+        System.out.println("=============");
+        System.out.println("Passando por todo a ListaLigada:");
+        pessoas.forEach(pessoa -> System.out.println(pessoa));
+    }
 }
