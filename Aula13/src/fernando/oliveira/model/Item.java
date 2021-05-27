f284024c5bf5847fcd87261c4ce2eabd0e8bde33
Fernando <ferd.olvr@gmail.com>
2021-05-27 08:33:33 -0300

    Item class

--- Aula13/src/fernando/oliveira/model/Item.java
+++ Aula13/src/fernando/oliveira/model/Item.java
@@ -1,4 +1,31 @@
 package fernando.oliveira.model;
 
+/**
+ * Classe que representa um item armazenado no sistema
+ */
 public class Item {
+    final public String nome;
+    final public double preco;
+
+    /**
+     * Construtor do elemento item
+     * @param nome
+     * @param preco
+     */
+    public Item(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    /**
+     * Método que representa o estado do objeto item
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Item{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
 }
