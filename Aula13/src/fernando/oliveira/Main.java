d9fdc9dc6701ba52ed6cb7339f95c975ed1298b5
Fernando <ferd.olvr@gmail.com>
2021-05-27 08:29:26 -0300

    try/catch main

--- Aula13/src/fernando/oliveira/Main.java
+++ Aula13/src/fernando/oliveira/Main.java
@@ -1,8 +1,27 @@
 package fernando.oliveira;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	File file = new File("dados.txt");
+    //Realizar a leitura do conteudo desse arquivo
+        try{
+            //criar elemento para iterar pelo arquivo
+            Scanner scanner = new Scanner(file);
+            //Passa por todos os elementos do arquivo
+            while(scanner.hasNext()){
+                String linha = scanner.nextLine();
+                System.out.println("Linha Atual:" + linha);
+            }
+
+        }
+        catch(Exception exception){
+            System.out.println("Algo deu errado"
+            );
+        }
     }
+
 }
