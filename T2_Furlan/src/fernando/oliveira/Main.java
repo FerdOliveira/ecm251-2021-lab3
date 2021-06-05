8c4c769bbedbbfe6bbd6df9728f5ecbabaedee62
Fernando <ferd.olvr@gmail.com>
2021-06-05 15:44:55 -0300

    Teste

--- T2_Furlan/src/fernando/oliveira/Main.java
+++ T2_Furlan/src/fernando/oliveira/Main.java
@@ -6,12 +6,12 @@ public class Main {
         Account c1 = new Account(0.0);
         System.out.println("C1: R$ "+ c1.getBalance());
         Cliente cliente1 =  new Cliente("GutaMax", c1);
-//        Cliente cliente2 =  new Cliente("Ferd", c1);
-//        Cliente cliente3 =  new Cliente("Rapha", c1);
-//        Cliente cliente4 =  new Cliente("Matheus", c1);
+        Cliente cliente2 =  new Cliente("Ferd", c1);
+        Cliente cliente3 =  new Cliente("Rapha", c1);
+        Cliente cliente4 =  new Cliente("Matheus", c1);
         cliente1.start();
-//        cliente2.start();
-//        cliente3.start();
-//        cliente4.start();
+        cliente2.start();
+        cliente3.start();
+        cliente4.start();
     }
 }
