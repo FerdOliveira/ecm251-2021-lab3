92a94ca9a319fc9036a24ce459bb65bc622c686e
Fernando <ferd.olvr@gmail.com>
2021-06-05 14:00:58 -0300

    Update Account.java

--- T2_Furlan/src/fernando/oliveira/Account.java
+++ T2_Furlan/src/fernando/oliveira/Account.java
@@ -1,4 +1,14 @@
 package fernando.oliveira;
 
 public class Account {
+    private double balance;
+
+    public Account(double saldoInicial){
+        this.balance = saldoInicial;
+    }
+
+    public void deposit(Double deposit){
+        balance += deposit;
+        System.out.println("Deposito efetuado: R$ " + deposit);
+    }
 }
