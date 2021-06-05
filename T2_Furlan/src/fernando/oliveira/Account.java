f77273838253e0266f7d44bfbb7f5a795aa027bf
Fernando <ferd.olvr@gmail.com>
2021-06-05 15:18:32 -0300

    withDraw

--- T2_Furlan/src/fernando/oliveira/Account.java
+++ T2_Furlan/src/fernando/oliveira/Account.java
@@ -1,15 +1,35 @@
 package fernando.oliveira;
 
+import java.util.Random;
+
+//@AllArgsConstructor
+//@Getter
 public class Account {
+    public double getBalance() {
+        return balance;
+    }
+
     private double balance;
 
     public Account(double saldoInicial){
         this.balance = saldoInicial;
-        System.out.println("Seu saldo inicial é de R$ "+ this.balance);
     }
 
     public void deposit(Double deposit){
         balance += deposit;
         System.out.println("Deposito efetuado: R$ " + deposit);
+    }
+    public int randomValores(){
+        Random v1 = new Random();
+        return v1.nextInt(3);
+
+    }
+    public void withdraw(Double withdraw){
+        if(balance>withdraw){
+            balance -= withdraw;
+            System.out.println("Foi retirado: R$- "+ withdraw);
+        }else{
+            System.out.println("Saldo Indisponivel");
+        }
     }
 }
--- T2_Furlan/src/fernando/oliveira/Main.java
+++ T2_Furlan/src/fernando/oliveira/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Account c1 = new Account(0.0);
+        c1.getBalance();
     }
 }
