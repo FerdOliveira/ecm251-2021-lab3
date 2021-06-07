4111814d41c24ccf9ddc871adae88db85d4e76ce
Fernando <ferd.olvr@gmail.com>
2021-06-06 21:38:25 -0300

    Update Main.java

--- T2_Furlan/src/fernando/oliveira/Main.java
+++ T2_Furlan/src/fernando/oliveira/Main.java
@@ -1,5 +1,8 @@
 package fernando.oliveira;
-
+//Fernando Oliveira de Souza 19.00617-9
+//Gustavo Zamboni do Carmo 19.01266-7
+//Matheus Takahashi Nakai 19.01355-8
+//Raphael Marchetti Calciolari 19.00828-7
 public class Main {
 
     public static void main(String[] args) {
