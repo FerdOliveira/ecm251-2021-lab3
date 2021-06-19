863a4120589925563135bda609fe26e195a63206
Fernando <ferd.olvr@gmail.com>
2021-06-19 19:30:30 -0300

    Update GenericRootNode.java

--- Estruturas11/src/GenericRootNode.java
+++ Estruturas11/src/GenericRootNode.java
@@ -1,2 +1,13 @@
-public class GenericRootNode {
+public class GenericRootNode extends GenericNode{
+
+    private int size;
+
+    public GenericRootNode(int data, String path,int size) {
+        super(data, path);
+        this.size = size;
+    }
+
+    public int getSize(){
+        return this.size;
+    }
 }
