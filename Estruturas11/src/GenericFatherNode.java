9c204569cec6198b23c9049a882a8fab6e30ad21
Fernando <ferd.olvr@gmail.com>
2021-06-19 19:52:31 -0300

    Create GenericFatherNode.java

---
+++ Estruturas11/src/GenericFatherNode.java
@@ -0,0 +1,9 @@
+public class GenericFatherNode extends GenericNode{
+
+    private int size;
+
+    public GenericFatherNode(int tamanho, String path,int size) {
+        super(tamanho, path);
+        this.size = size;
+    }
+}
