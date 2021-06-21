6863b6b122750757d676343cb74f10b06fc8f0aa
Fernando <ferd.olvr@gmail.com>
2021-06-21 15:54:19 -0300

    Test1_TelaInicial

--- ATIVIDADE2/src/fernando/oliveira/Main.java
+++ ATIVIDADE2/src/fernando/oliveira/Main.java
@@ -1,8 +1,10 @@
 package fernando.oliveira;
 
+import fernando.oliveira.sistema.Menu;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Menu.menu();
     }
 }
