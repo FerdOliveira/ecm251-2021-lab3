7df3252270f2e91af43894f57f9b0ba8d6eae5ac
Fernando <ferd.olvr@gmail.com>
2021-06-21 15:49:23 -0300

    Update Menu.java
    TelaDeBoasVindas

--- ATIVIDADE2/src/fernando/oliveira/sistema/Menu.java
+++ ATIVIDADE2/src/fernando/oliveira/sistema/Menu.java
@@ -1,4 +1,8 @@
 package fernando.oliveira.sistema;
 
 public class Menu {
+    public static void menu(){
+        System.out.println("Opções Disponiveis: ");
+        System.out.println(" 1 - Castrar um novo usuário \n 2 - Postar uma mensagem \n 3 - Trocar o horário de trabalho \n 4 - Excluir usuário \n 5 - Para sair ");
+    }
 }
