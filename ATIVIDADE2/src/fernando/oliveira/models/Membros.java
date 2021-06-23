220d386d7a66500e6aa8d0ab98ad7bb921d89629
Fernando <ferd.olvr@gmail.com>
2021-06-23 16:03:36 -0300

    Update Membros.java
    Adicionado construtor e um to string

--- ATIVIDADE2/src/fernando/oliveira/models/Membros.java
+++ ATIVIDADE2/src/fernando/oliveira/models/Membros.java
@@ -1,4 +1,23 @@
 package fernando.oliveira.models;
 
 public class Membros {
+    private String nome;
+    private String email;
+    private String cargo;
+
+    public Membros(String nome, String email, String cargo){
+        this.nome = nome;
+        this.email = email;
+        this.cargo = cargo;
+    }
+
+    @Override
+    public String toString() {
+        return "Membros{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", cargo='" + cargo + '\'' +
+                '}';
+    }
+    // Se nao tiver esse override ele printa assim a lista de membros "fernando.oliveira.models.Membros@9629756"
 }
