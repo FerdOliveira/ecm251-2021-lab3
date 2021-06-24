911e0493ddfc567e199a6a251b30ba195ba26be0
Fernando <ferd.olvr@gmail.com>
2021-06-24 14:23:01 -0300

    Update Menu.java
    Adicionado o horário do sistema no programa e o case para quando ele quiser trocar de horário

--- ATIVIDADE2/src/fernando/oliveira/sistema/Menu.java
+++ ATIVIDADE2/src/fernando/oliveira/sistema/Menu.java
@@ -1,20 +1,31 @@
 package fernando.oliveira.sistema;
 
+import fernando.oliveira.enums.HorarioSistema;
 import fernando.oliveira.models.Membros;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
+
     private static ArrayList<Membros> listaUsuarios = new ArrayList<>(); //ArrayList foi escolhido por apresentar o que mais preciso https://lemus.webs.upv.es/wordpress/wp-content/uploads/2018/02/cheat-sheets.pdf
+    private static HorarioSistema horarioSistema = HorarioSistema.NORMAL;
 
     public static void menu() {
         boolean op = true;
+
         while (op) {
+            if (horarioSistema.equals(HorarioSistema.NORMAL)){
+                System.out.println("HORÁRIO NORMAL");
+            }else{
+                System.out.println("HORÁRIO EXTRA");
+            }
             System.out.println("Opções Disponiveis: ");
             System.out.println(" 1 - Castrar um novo usuário \n 2 - Postar uma mensagem \n 3 - Trocar o horário de trabalho \n 4 - Excluir usuário \n 5 - Apresentacao de todos os membros \n 6 - Para sair ");
             Scanner in = new Scanner(System.in);
+
             int num = in.nextInt();
+
             switch (num) {
                 case 1:
                     System.out.println("Opcao de cadastrar escolhida");
@@ -48,6 +59,15 @@ public class Menu {
                             break;
                     }
                     break;
+                case 3:
+                    if (horarioSistema.equals(horarioSistema.NORMAL)){
+                        horarioSistema = horarioSistema.EXTRA;
+                        System.out.println("Voce agora esta fazendo hora EXTRA");
+                    }else{
+                        horarioSistema = horarioSistema.NORMAL;
+                        System.out.println("Seu horario agora é NORMAL");
+                    }
+                    break;
                 case 4:
                     System.out.println("Digite a posição do membro cadastrado que deseja excluir: ");
                     int excluirMembro = in.nextInt();
@@ -62,6 +82,7 @@ public class Menu {
                         index++;
                     }
                     break;
+
                 case 6:
                     listaUsuarios.forEach(System.out::println);
                     System.out.println("Obrigado por usar nosso sistema");
