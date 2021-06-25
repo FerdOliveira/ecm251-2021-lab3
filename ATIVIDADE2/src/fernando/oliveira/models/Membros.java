a0b7e5c7fce188ccad9f81e8e02421b8b7ae419f
Fernando <ferd.olvr@gmail.com>
2021-06-25 18:44:49 -0300

    Update Membros.java

--- ATIVIDADE2/src/fernando/oliveira/models/Membros.java
+++ ATIVIDADE2/src/fernando/oliveira/models/Membros.java
@@ -1,9 +1,13 @@
 package fernando.oliveira.models;
 
 import fernando.oliveira.Interface.ApresentacaoMembros;
+import fernando.oliveira.Interface.PostarMensagem;
+import fernando.oliveira.enums.HorarioSistema;
 import fernando.oliveira.enums.TiposDeMembro;
 
-public class Membros implements ApresentacaoMembros {
+import java.sql.SQLOutput;
+
+public abstract class Membros implements ApresentacaoMembros, PostarMensagem {
 
     //TODO: tentar usar o lombok para os getters e construtor
     public String getNome() {
@@ -16,17 +20,25 @@ public class Membros implements ApresentacaoMembros {
 
     public TiposDeMembro getCargo() { return cargo; }
 
+    public String getExtraAssinatura() { return extraAssinatura; }
+
+    public String getNormalAssinatura() { return normalAssinatura; }
+
     protected String nome;
     protected String email;
     protected TiposDeMembro cargo;
+    protected String extraAssinatura = "";
+    protected String normalAssinatura = "";
 
-    public Membros(String nome, String email, TiposDeMembro cargo){
+    public Membros(String nome, String email, TiposDeMembro cargo) {
         this.nome = nome;
         this.email = email;
         this.cargo = cargo;
+        this.extraAssinatura = cargo.getExtraAssinatura();
+        this.normalAssinatura = cargo.getNormalAssinatura();
     }
 
-    public void apresentacao(){
+    public void apresentacao() {
         System.out.println("Nome: " + getNome() + "\n" + "Email: " + getEmail() + "\n" + "Cargo: " + getCargo() + "\n-----------------------");
     }
 
@@ -37,6 +49,16 @@ public class Membros implements ApresentacaoMembros {
                 ", email='" + email + '\'' +
                 ", cargo='" + cargo + '\'' +
                 '}';
+    }// Se nao tiver esse override ele printa assim a lista de membros "fernando.oliveira.models.Membros@9629756"
+
+    public void postarMensagem(String mensagem, HorarioSistema horarioSistema, TiposDeMembro membro) {
+        System.out.println("Enviando mensagem: ");
+        if (horarioSistema.EXTRA.equals(horarioSistema)){
+            System.out.println( mensagem + "\nAss: " + membro.getExtraAssinatura());
+        }else{
+            System.out.println( mensagem + "\nAss: " + membro.getNormalAssinatura());
+        }
+        System.out.println("---------------------------------------------");
     }
-    // Se nao tiver esse override ele printa assim a lista de membros "fernando.oliveira.models.Membros@9629756"
+
 }
