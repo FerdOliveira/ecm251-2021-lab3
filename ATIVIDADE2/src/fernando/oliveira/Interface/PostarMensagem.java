317bf62385c121f5b07f194d9bfba9bab59e484b
Fernando <ferd.olvr@gmail.com>
2021-06-25 18:45:03 -0300

    Update PostarMensagem.java

--- ATIVIDADE2/src/fernando/oliveira/Interface/PostarMensagem.java
+++ ATIVIDADE2/src/fernando/oliveira/Interface/PostarMensagem.java
@@ -1,5 +1,8 @@
 package fernando.oliveira.Interface;
 
+import fernando.oliveira.enums.HorarioSistema;
+import fernando.oliveira.enums.TiposDeMembro;
+
 public interface PostarMensagem {
-    public void postarMensagem();
+    public void postarMensagem(String mensagem, HorarioSistema horarioSistema, TiposDeMembro membro);
 }
