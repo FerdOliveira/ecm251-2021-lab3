ccbf1680ba10d642a607e4dbfde6249669d14bd8
Fernando <ferd.olvr@gmail.com>
2021-06-25 18:44:59 -0300

    Update HeavyLifters.java

--- ATIVIDADE2/src/fernando/oliveira/models/HeavyLifters.java
+++ ATIVIDADE2/src/fernando/oliveira/models/HeavyLifters.java
@@ -2,8 +2,9 @@ package fernando.oliveira.models;
 
 import fernando.oliveira.enums.TiposDeMembro;
 
-public class HeavyLifters extends Membros{
+public class HeavyLifters extends Membros {
     public HeavyLifters(String nome, String email, TiposDeMembro cargo) {
         super(nome, email, cargo);
     }
+
 }
