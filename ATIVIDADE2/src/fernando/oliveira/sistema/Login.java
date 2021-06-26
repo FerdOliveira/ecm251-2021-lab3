0c4ba5011044f529ed10314806ec5e67c4635af2
Fernando <ferd.olvr@gmail.com>
2021-06-26 15:23:57 -0300

    Update Login.java

--- ATIVIDADE2/src/fernando/oliveira/sistema/Login.java
+++ ATIVIDADE2/src/fernando/oliveira/sistema/Login.java
@@ -2,10 +2,13 @@ package fernando.oliveira.sistema;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
+import static fernando.oliveira.sistema.Menu.menu;
+
 public class Login {
-    public static void run() throws FileNotFoundException {
+    public static void run() throws IOException {
         Scanner scan = new Scanner(new File("C:\\projeto\\LoginBigBrother.txt")); // pegar o arquivo do meu repositorio do github
         Scanner keyboard = new Scanner(System.in);
         String user = scan.nextLine();
@@ -16,7 +19,7 @@ public class Login {
 
         if (inpUser.equals(user) && inpPass.equals(pass)) {
             System.out.print("Bem vindo BigBrother \n");
-            Menu.menu();
+            menu();
         } else {
             System.out.print("VOCE NAO É UM BIG BROTHER, LOGO NAO PODERA ACESSAR MEUS RECURSOS");
         }
