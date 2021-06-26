0865275858769769972ffd79fab4255166fde0eb
Fernando <ferd.olvr@gmail.com>
2021-06-26 15:24:04 -0300

    Update Main.java

--- ATIVIDADE2/src/fernando/oliveira/Main.java
+++ ATIVIDADE2/src/fernando/oliveira/Main.java
@@ -3,9 +3,10 @@ package fernando.oliveira;
 import fernando.oliveira.sistema.Login;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         Login.run();
     }
 }
