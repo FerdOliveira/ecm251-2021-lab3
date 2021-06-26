c47382b31126eaa324f3daea69a9be3db2a4dce5
Fernando <ferd.olvr@gmail.com>
2021-06-26 15:55:40 -0300

    Update ApresentacaoMembros.java

--- ATIVIDADE2/src/fernando/oliveira/Interface/ApresentacaoMembros.java
+++ ATIVIDADE2/src/fernando/oliveira/Interface/ApresentacaoMembros.java
@@ -1,5 +1,8 @@
 package fernando.oliveira.Interface;
 
+/**
+ * Interface de Apresentacao de todos os membros, implementada em membros
+ */
 public interface ApresentacaoMembros {
     public void apresentacao();
 }
