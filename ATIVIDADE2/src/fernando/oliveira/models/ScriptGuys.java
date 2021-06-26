6a0587bdc1803b027cb32c9006243ab1dbc70973
Fernando <ferd.olvr@gmail.com>
2021-06-26 15:55:22 -0300

    Update ScriptGuys.java

--- ATIVIDADE2/src/fernando/oliveira/models/ScriptGuys.java
+++ ATIVIDADE2/src/fernando/oliveira/models/ScriptGuys.java
@@ -2,9 +2,12 @@ package fernando.oliveira.models;
 
 import fernando.oliveira.enums.TiposDeMembro;
 
+/**
+ * Classe filha de Membros
+ */
+
 public class ScriptGuys extends Membros {
     public ScriptGuys(String nome, String email, TiposDeMembro cargo) {
         super(nome, email, cargo);
     }
-
 }
