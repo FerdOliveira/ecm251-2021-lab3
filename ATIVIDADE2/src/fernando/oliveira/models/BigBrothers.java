e2277732733338fb4c4ee19a04dab52f58500e74
Fernando <ferd.olvr@gmail.com>
2021-06-26 15:55:30 -0300

    Update BigBrothers.java

--- ATIVIDADE2/src/fernando/oliveira/models/BigBrothers.java
+++ ATIVIDADE2/src/fernando/oliveira/models/BigBrothers.java
@@ -2,7 +2,11 @@ package fernando.oliveira.models;
 
 import fernando.oliveira.enums.TiposDeMembro;
 
-public class BigBrothers extends Membros{
+/**
+ * Classe filha de Membros
+ */
+
+public class BigBrothers extends Membros {
     public BigBrothers(String nome, String email, TiposDeMembro cargo) {
         super(nome, email, cargo);
     }
