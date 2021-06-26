02baded89882b511f8f054ac5642b2362de2f648
Fernando <ferd.olvr@gmail.com>
2021-06-26 15:55:24 -0300

    Update MobileMembers.java

--- ATIVIDADE2/src/fernando/oliveira/models/MobileMembers.java
+++ ATIVIDADE2/src/fernando/oliveira/models/MobileMembers.java
@@ -2,20 +2,12 @@ package fernando.oliveira.models;
 
 import fernando.oliveira.enums.TiposDeMembro;
 
+/**
+ * Classe filha de Membros
+ */
+
 public class MobileMembers extends Membros {
     public MobileMembers(String nome, String email, TiposDeMembro cargo) {
         super(nome, email, cargo);
     }
-
-    @Override
-    public String getExtraAssinatura() {
-        return super.getExtraAssinatura();
-    }
-
-    @Override
-    public String getNormalAssinatura() {
-        return super.getNormalAssinatura();
-    }
-
-
 }
