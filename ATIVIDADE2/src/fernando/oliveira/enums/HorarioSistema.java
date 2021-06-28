cae3bbf8dbd9569c21857bf424c32a19cc224a9f
Fernando <ferd.olvr@gmail.com>
2021-06-27 22:42:30 -0300

    Update HorarioSistema.java

--- ATIVIDADE2/src/fernando/oliveira/enums/HorarioSistema.java
+++ ATIVIDADE2/src/fernando/oliveira/enums/HorarioSistema.java
@@ -4,5 +4,19 @@ package fernando.oliveira.enums;
  * Enum para determinar se o horário do sistema é EXTRA ou NORMAL
  */
 public enum HorarioSistema {
-    EXTRA, NORMAL;
+    EXTRA {
+        @Override
+        public HorarioSistema trocarHorario() {
+            System.out.println("Seu horario agora é NORMAL");
+            return NORMAL;
+        }
+    }, NORMAL {
+        @Override
+        public HorarioSistema trocarHorario() {
+            System.out.println("Voce agora esta fazendo hora EXTRA");
+            return EXTRA;
+        }
+    };
+
+    public abstract HorarioSistema trocarHorario();
 }
