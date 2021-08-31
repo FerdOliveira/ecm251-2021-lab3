b7c311fdea625fc7cf09766cc7bb6db817396df3
Fernando <ferd.olvr@gmail.com>
2021-08-30 22:13:28 -0300

    Update pegarBaralho.kt

--- teste.kt/src/main/java/pegarBaralho.kt
+++ teste.kt/src/main/java/pegarBaralho.kt
@@ -1,2 +1,13 @@
-class pegarBaralho {
+import controller.DeckController
+import model.Card
+import org.json.JSONArray
+import org.json.JSONObject
+import java.sql.DriverManager.println
+
+fun main() {
+    val deckController = DeckController()
+    val carta = deckController.drawCard()
+    println(carta)
+    val carta2 = deckController.drawCard()
+    println(carta2)
 }
