8dea9ae1cc8157f61deaaadc372d14f1417e7dc8
Fernando <ferd.olvr@gmail.com>
2021-10-03 22:39:25 -0300

    Create App.java

---
+++ Atividade17_DISPY/Atividade17/src/App.java
@@ -0,0 +1,12 @@
+/**
+ * @author Gustavo Zamboni do Carmo 19.01266-7
+ * @author Raphael Marchetti Calciolari 19.00828-7
+ * @author Fernando Oliveira de Souza 19.00617-9
+ * @author Matheus Takahashi Nakai 19.01355-8
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
